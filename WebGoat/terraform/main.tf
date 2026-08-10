01aff723d80222ee2163f734cc223ab2856bd1bc
Rupsha <saharupsha18@gmail.com>
2026-08-11 00:52:34 +0530

    Add vulnerable Terraform file for Checkov scan

---
+++ WebGoat/terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "my-insecure-demo-bucket"
+}
+resource "aws_s3_bucket_public_access_block" "demo_block" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_acl" "demo_acl" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  acl    = "public-read"
+}
